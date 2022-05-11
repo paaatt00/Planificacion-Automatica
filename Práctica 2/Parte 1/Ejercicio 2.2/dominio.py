c348b2fbe9c3016f202c4c9c02ab10600e99e2a5
Isabel Blanco <isabel.blanco.martinez@gmail.com>
2022-05-11 13:21:49 +0200

    send boxes

--- Práctica 2/Parte 1/Ejercicio 2.2/dominio.py
+++ Práctica 2/Parte 1/Ejercicio 2.2/dominio.py
@@ -19,7 +19,9 @@ def drop(state, d, b, c, l, h):
     if (drone_at[d] == l) and (drone_carry[d] == b) and (human_at[h] == l) and (box_has[b] == c):
         state.drone_carry[d] = False
         state.box_free = False
-        del state.human_needs[h] = c #¿Como hacemos para borrar solo el contenido de esa caja?
+        state.human_needs[h].remove(c) # Borrar contenido especifico de un humano
+        if (len(state.human_needs[h]) == 0):
+            state.human_needs.pop(h)
         state.drone_free[d] = True
         state.box_at[b] = l
         return state
@@ -27,7 +29,26 @@ def drop(state, d, b, c, l, h):
 
 # Métodos
 def send_box(state, h, c):
-    if (state.box_at[h] == ) 
+    box_keys = state.box_has.keys()
+    drone_keys = state.drone_at.keys()
+    d = drone_keys[0] # Tenemos el drone
+    box_found = False
+
+    for i in range len(box_keys):
+        if box_keys[i] == c:
+            b = state.box_has[i] # Obtenemos la caja en la variable b
+            box_found = True
+            break
+    if (box_found == True) and (state.box_at[b] == 'warehouse'):
+        to = state.human_at[h] # Localizacion a la que tiene que ir el drone
+        f = 'warehouse' # From
+        pick_up(state, d, b, c, f)
+        move(state, d, f, to)
+        drop(state, d, b, c, to, h)
+        move(state, d, to, f)
+        return state
+    else:
+        return False
 
 def send_all_boxes(state):
     if (state.human_needs[h] == c)
@@ -36,26 +57,18 @@ hop.declare_operators(move, pick_up, drop)
 hop.declare_methods(send_box, send_all_boxes)
 
 # Estado inicial
-class State:
-    def __init__(self, loc, drone_at, box_at, drone_free, box_has, box_free, drone_carry, human_at, human_needs):
-        self.loc = loc
-        self.drone_at = drone_at
-        self.box_at = box_at
-        self.drone_free = drone_free
-        self.box_has = box_has
-        self.box_free = box_free
-        self.drone_carry = drone_carry
-        self.human_at = human_at
-        self.human_needs = human_needs
-    
-state1 = State('state1')
-state1.loc = {'drone1': 'warehouse'} #¿y el resto de cajas y localizaciones como se declaran?
-state1.drone_at = {'drone' : 'warehouse'}
-state1.box_at = {'' : ''}
-state1.drone_free = {'' : ''}
-state1.box_has = {'' : ''}
-state1.box_free = {'' : ''}
-state1.drone_carry = {'' : ''}
-state1.human_at = human_at = {'' : ''}
-state1.human_needs = human_needs = {'' : ''}
+state1 = hop.State('state1')
+state1.drone_at = {'drone1' : 'warehouse'}
+state1.box_at = {'box1': 'warehouse',
+                'box2': 'warehouse'}
+state1.drone_free = {'drone1' : True}
+state1.box_has = {'meds' : 'box1',
+                    'food' : 'box2'}
+state1.box_free = {'box1' : True,
+                    'box2' : True }
+state1.drone_carry = {'drone1' : False}
+state1.human_at = {'human1' : 'loc1'
+                                'human2' : 'loc2'}
+state1.human_needs = {'human1' : 'food', #[ 'meds', 'food']
+                        'human2':  'meds'}
 
